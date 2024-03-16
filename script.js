eaa71554be9a24d79b38ecdba61ff321a4723824
Kuldeep kumar <117909055+kd-234@users.noreply.github.com>
2024-03-16 12:24:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function fibonacci(num) {
-// your code here
-}
+    let a = 0, b = 1, temp;
 
+    if (num === 1) {
+        return a;
+    }
+
+    for (let i = 2; i <= num; i++) {
+        temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return a;
+}
+}
 module.exports = fibonacci;
