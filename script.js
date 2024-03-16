5d553d39d7e26c937ac56d031f15aeeae7d255a9
Kuldeep kumar <117909055+kd-234@users.noreply.github.com>
2024-03-16 14:27:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 function fibonacci(num) {
-    let a = 0, b = 1, temp;
 
-    if (num === 1) {
-        return a;
-    }
-    for (let i = 2; i <= num; i++) {
-        temp = a + b;
-        a = b;
-        b = temp;
-    }
-    return a;
-	
+let a = 0, b = 1, temp;
+
+if (num === 1 ||num==0) {
+
+return 0;
 }
+
+for (let i = 2; i <= num; i++) {
+
+temp = a + b;
+
+a = b;
+
+b = temp;
 }
-document.getElementById("table1").innerHTML=a;
-module.exports = fibonacci;
+
+return a;
+
+}
+
+module.exports = fibonacci ;
